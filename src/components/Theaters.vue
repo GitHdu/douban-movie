f0b1eab40ca973d78d5b8d7e39a1dbefb62919bd
myfly2013 <myfly2013@yeah.net>
2018-05-17 22:12:26 +0800

    refactor:

--- src/components/Theaters.vue
+++ src/components/Theaters.vue
@@ -1,33 +1,50 @@
 <template>
-<div class="container-fluid">
+    <div class="container-fluid">
         <div class="row">
             <div class="col-sm-3 col-md-2 sidebar">
                 <ul class="nav nav-sidebar">
-                    <router-link to="/in_theaters" tag="li" active-class="active" class="active">
+                    <router-link to="/in_theaters"
+                                 tag="li"
+                                 active-class="active"
+                                 class="active">
                         <a href="javascript:;">正在热映</a>
                     </router-link>
-                    <router-link to="/coming_soon?start=0&count=10" tag="li" active-class="active">
+                    <router-link to="/coming_soon?start=0&count=10"
+                                 tag="li"
+                                 active-class="active">
                         <a href="javascript:;">即将上映</a>
                     </router-link>
-                    <router-link to="/top250" tag="li" active-class="active">
+                    <router-link to="/top250"
+                                 tag="li"
+                                 active-class="active">
                         <a href="javascript:;">TOP250</a>
                     </router-link>
                 </ul>
             </div>
-            <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main" >
-                <h2 class="page-header" v-text="movies.title"></h2>
+            <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+                <h2 class="page-header"
+                    v-text="movies.title"></h2>
                 <div class="list-group">
-                    <router-link :to="'/subject/'+item.id" v-for="(item,index) in movies.subjects" :key="item.id" active-class="active" >
-                        <a class="list-group-item" href="javascript:;">
+                    <router-link :to="'/subject/'+item.id"
+                                 v-for="(item,index) in movies.subjects"
+                                 :key="item.id"
+                                 active-class="active">
+                        <a class="list-group-item"
+                           href="javascript:;">
                             <span class="badge">{{ item.rating.average }}</span>
                             <div class="media">
                                 <div class="media-left">
-                                    <img class="media-object" :src="item.images.small" >
+                                    <img class="media-object"
+                                         :src="item.images.small">
                                 </div>
                                 <div class="media-body">
                                     <h3 class="media-heading">{{ item.title }}</h3>
-                                    <p>导演：<span v-for="director in item.directors">{{ director.name }}</span></p>
-                                    <p>类型：<span>{{ item.genres.join('、') }}</span></p>
+                                    <p>导演：
+                                        <span v-for="director in item.directors">{{ director.name }}</span>
+                                    </p>
+                                    <p>类型：
+                                        <span>{{ item.genres.join('、') }}</span>
+                                    </p>
                                 </div>
                             </div>
                         </a>
@@ -41,24 +58,24 @@
 <script>
 import axios from 'axios'
 export default {
-  name:"Theaters",
-  data(){
-    return {
-      movies:{}
-    }
-  },
-  mounted(){
-    this.getData()
-  },
-  methods:{
-    getData(){
-      var _this=this;
-      axios.get('/movie/in_theaters').then(function(res){
-        //console.log(res)
-        _this.movies = res.data
+    name: "Theaters",
+    data() {
+        return {
+            movies: {}
+        }
+    },
+    mounted() {
+        this.getData()
+    },
+    methods: {
+        getData() {
+            var _this = this;
+            axios.get('/movie/in_theaters').then(function (res) {
+                //console.log(res)
+                _this.movies = res.data
 
-      })
+            })
+        }
     }
-  }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,39 +1,45 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import axios from 'axios'
-import './assets/css/bootstrap.min.css'
-import './assets/js/bootstrap.min'
-import './assets/css/app.css'
-import Loading from './components/Loading'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import axios from 'axios';
+import './assets/css/bootstrap.min.css';
+import './assets/js/bootstrap.min';
+import './assets/css/app.css';
+import Loading from './components/Loading';
 
-Vue.config.productionTip = false
-Vue.use(Loading);
-axios.interceptors.request.use((config) => { //配置发送请求的信息
-    Vue.set(vm.$data,'loading',true)
+Vue.config.productionTip = false;
+Vue.use (Loading);
+axios.interceptors.request.use (
+  config => {
+    //配置发送请求的信息
+    Vue.set (vm.$data, 'loading', true);
     return config;
-}, function(error) {
-    return Promise.reject(error);
-});
+  },
+  function (error) {
+    return Promise.reject (error);
+  }
+);
 
-axios.interceptors.response.use((response) =>{ //配置请求回来的信息
-    Vue.set(vm.$data,'loading',false)
+axios.interceptors.response.use (
+  response => {
+    //配置请求回来的信息
+    Vue.set (vm.$data, 'loading', false);
     return response;
-}, function(error) {
-    return Promise.reject(error);
-});
+  },
+  function (error) {
+    return Promise.reject (error);
+  }
+);
 
 /* eslint-disable no-new */
-var vm=new Vue({
+var vm = new Vue ({
   el: '#app',
-  data:{
-    loading:true
+  data: {
+    loading: true,
   },
   router,
   template: '<App :child-msg="loading"/>',
-  components: { App }
-})
-
-
+  components: {App},
+});
